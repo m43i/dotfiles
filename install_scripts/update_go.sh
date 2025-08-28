59de359917e1a71ab660d930e6a1e8b8ddc74e2d
m43i <36700796+m43i@users.noreply.github.com>
2025-08-28 16:20:29 +0200

    feat: use go 1.25.0

--- install_scripts/update_go.sh
+++ install_scripts/update_go.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 sudo rm -rf /usr/local/go
-curl -L -O https://go.dev/dl/go1.24.5.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf go1.24.5.linux-amd64.tar.gz
-rm go1.24.5.linux-amd64.tar.gz
+curl -L -O https://go.dev/dl/go1.25.0.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.25.0.linux-amd64.tar.gz
+rm go1.25.0.linux-amd64.tar.gz
