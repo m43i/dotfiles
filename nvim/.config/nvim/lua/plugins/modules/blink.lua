360e6d17334a1b6f87bc39fcc2c2e96b643925fd
Malte Eiting <malte.eiting@icloud.com>
2026-01-28 08:42:53 +0100

    feat: copilot lsp

--- nvim/.config/nvim/lua/plugins/modules/blink.lua
+++ nvim/.config/nvim/lua/plugins/modules/blink.lua
@@ -14,7 +14,7 @@ return {
 		completion = {
 			accept = {
 				auto_brackets = {
-					enabled = true,
+					enabled = false,
 				},
 			},
 			menu = {
--- nvim/.config/nvim/lua/plugins/modules/copilot.lua
+++ nvim/.config/nvim/lua/plugins/modules/copilot.lua
@@ -1,12 +1,36 @@
 return {
 	"zbirenbaum/copilot.lua",
-    dependencies = {
-        "copilotlsp-nvim/copilot-lsp",
-    },
+	dependencies = {
+		"copilotlsp-nvim/copilot-lsp",
+	},
 	config = function()
 		require("copilot").setup({
 			panel = { enable = false },
 			suggestion = { enabled = false },
+			nes = {
+                enable = false,
+				auto_trigger = false,
+				keymap = {
+					accept = "<Tab>",
+					dismiss = "<Esc>",
+				},
+			},
+		})
+
+		-- Hide copilot suggestions when blink menu opens
+		vim.api.nvim_create_autocmd("User", {
+			pattern = "BlinkCmpMenuOpen",
+			callback = function()
+				vim.b.copilot_suggestion_hidden = true
+			end,
+		})
+
+		-- Show copilot suggestions when blink menu closes
+		vim.api.nvim_create_autocmd("User", {
+			pattern = "BlinkCmpMenuClose",
+			callback = function()
+				vim.b.copilot_suggestion_hidden = false
+			end,
 		})
 	end,
 }
